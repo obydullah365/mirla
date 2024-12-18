3824313b12beaec74cca3a42a60915792991d506
obydullah365 <md.obydullah365@gmail.com>
2024-12-18 22:31:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+    document.querySelector('nav').classList.toggle('open');
+});
+
 document.getElementById('learn-more-btn').addEventListener('click', function() {
     alert('More details coming soon!');
 });
