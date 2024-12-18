71142c143020e6fd999ebe753a89e21b65d735d1
obydullah365 <md.obydullah365@gmail.com>
2024-12-18 22:34:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Toggle the mobile navigation menu when the hamburger icon is clicked
 document.getElementById('hamburger-btn').addEventListener('click', function() {
     document.querySelector('nav').classList.toggle('open');
 });
 
+// Example: Show alert when "Learn More" button is clicked
 document.getElementById('learn-more-btn').addEventListener('click', function() {
     alert('More details coming soon!');
 });
